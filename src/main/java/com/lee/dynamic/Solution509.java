5a7f56b0e4a234deccb9afc98b5158d0d39dbe20
He Even <wqhyw@126.com>
2021-07-08 22:15:34 +0800

    dynamic fib

--- src/main/java/com/lee/dynamic/Solution509.java
+++ src/main/java/com/lee/dynamic/Solution509.java
@@ -1,13 +1,26 @@
 package com.lee.dynamic;
 
 public class Solution509 {
+//    public int fib(int n) {
+//        if (n == 0) {
+//            return 0;
+//        }
+//        if (n == 1) {
+//            return 1;
+//        }
+//        return fib(n - 1) + fib(n - 2);
+//    }
+
     public int fib(int n) {
-        if (n == 0) {
-            return 0;
+        if (n < 2) {
+            return n;
         }
-        if (n == 1) {
-            return 1;
+        int o = 0, p = 1, q = 0;
+        for (int i = 0; i < n - 1; i++) {
+            q = o + p;
+            o = p;
+            p = q;
         }
-        return fib(n - 1) + fib(n - 2);
+        return q;
     }
 }
