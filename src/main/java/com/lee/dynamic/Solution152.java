86d2d19eeb1490ced8dd6fe43ae90d7723cc3674
He Even <wqhyw@126.com>
2021-07-10 19:07:19 +0800

    dynamic 152

--- src/main/java/com/lee/dynamic/Solution152.java
+++ src/main/java/com/lee/dynamic/Solution152.java
@@ -3,6 +3,31 @@ package com.lee.dynamic;
 public class Solution152 {
     public int maxProduct(int[] nums) {
         // [2,3,-2,4]
-        return 0;
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        // init, till 2
+        int maxExcluded = nums[0];
+        int maxIncluded = Math.max(nums[0] * nums[1], nums[1]);
+        int minIncluded = Math.min(nums[0] * nums[1], nums[1]);
+
+        int previousMaxIncluded, previousMaxExcluded, previousMinIncluded;
+        for (int i = 2; i < nums.length; i++) {
+            int n = nums[i];
+            // not use this n
+            previousMaxIncluded = maxIncluded;
+            previousMaxExcluded = maxExcluded;
+            previousMinIncluded = minIncluded;
+
+            maxExcluded = Math.max(previousMaxIncluded, previousMaxExcluded);
+            // include
+            maxIncluded = Math.max(Math.max(previousMaxIncluded * n, n), previousMinIncluded * n);
+            // min
+            minIncluded = Math.min(Math.min(previousMinIncluded * n, n), previousMaxIncluded * n);
+        }
+        return Math.max(maxExcluded, maxIncluded);
     }
+    // leecode的题解更直观
+    // 算出以每个元素为结尾的最大值 （nums 长度个）放入一个数组
+    // 最后再遍历这个数组，选出最大值
 }
--- src/test/java/com/lee/dynamic/Solution152Test.java
+++ src/test/java/com/lee/dynamic/Solution152Test.java
@@ -1,20 +1,33 @@
 package com.lee.dynamic;
 
 import lombok.val;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class Solution152Test {
-
-    // 21:37
-    @Test
-    void test() {
+    @ParameterizedTest
+    @MethodSource("args")
+    void testA(int[] nums, int expectedMax) {
         val s = new Solution152();
         // when
-        val r = s.maxProduct(new int[]{2, 3, -2, 4});
+        val r = s.maxProduct(nums);
         // then
-        assertThat(r).isEqualTo(6);
+        assertThat(r).isEqualTo(expectedMax);
+
     }
+
+    private static Stream<Arguments> args() {
+        return Stream.of(
+                Arguments.of(new int[]{2, 3, -2, 4}, 6),
+                Arguments.of(new int[]{-2, 0, -1}, 0),
+                Arguments.of(new int[]{-2, 3, -4}, 24),
+                Arguments.of(new int[]{-1, -2, -9, -6}, 108)
+        );
+    }
+
 }
